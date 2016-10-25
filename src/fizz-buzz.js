dcf03553eaf626bf52fd908af2fd2adbb7117353
Jigar Gosar <jigar.gosar@gmail.com>
2016-10-25 16:57:42 +0530

    porting list functions

--- src/fizz-buzz.js
+++ src/fizz-buzz.js
@@ -12,22 +12,29 @@ function fizzBuzz(print) {
 
 }
 
-const ZERO      = (fn) => x => x
-const INCREMENT = NUM => fn => x => fn(NUM(fn)(x))
-const ADD       = NUM1 => NUM2 => NUM2(INCREMENT)(NUM1)
-const MULTIPLY  = m=>n=>n(ADD(m))(ZERO)
-
-const DECREMENT = NUMBER=>f=>x=>
-    NUMBER(g=>h=>h(g(f)))(y=>x)(y=>y)
-
-const SUBTRACT = m=>n=>n(DECREMENT)(m)
-
-//Z Combinator
-const Z = f=>(x=>f(y=>x(x)(y)))(x=>f(y=>x(x)(y)))
-
+const ZERO             = fn=>x=>x
+const INCREMENT        = n=>fn=>x=>fn(n(fn)(x))
+const ADD              = n => m => m(INCREMENT)(n)
+const MULTIPLY         = m=>n=>n(ADD(m))(ZERO)
+const DECREMENT        = n=>f=>x=> n(g=>h=>h(g(f)))(y=>x)(y=>y)
+const SUBTRACT         = m=>n=>n(DECREMENT)(m)
+const Z                = f=>(x=>f(y=>x(x)(y)))(x=>f(y=>x(x)(y))) //Z Combinator
 const IS_LESS_OR_EQUAL = m=>n=>IS_ZERO(SUBTRACT(m)(n))
-
-var DIVIDE = Z(f=>m=>n=>IF(IS_LESS_OR_EQUAL(n)(m))(x=>INCREMENT(f(SUBTRACT(m)(n))(n))(x))(ZERO))
+var DIVIDE             = Z(f=>m=>n=>IF(IS_LESS_OR_EQUAL(n)(m))(x=>INCREMENT(f(SUBTRACT(m)(n))(n))(x))(ZERO))
+var MOD                = Z(f=>m=>n=>IF(IS_LESS_OR_EQUAL(n)(m))(x=>f(SUBTRACT(m)(n))(n)(x))(m));
+//Lists
+const PAIR             = x=>y=>f=>f(x)(y)
+const LEFT             = p=>p(x=>y=>x)
+const RIGHT            = p=>p(x=>y=>y)
+const EMPTY            = PAIR(TRUE)(TRUE);
+const UNSHIFT          = l=>x=>PAIR(FALSE)(PAIR(x)(l));
+const IS_EMPTY         = LEFT;
+const FIRST            = l=>LEFT(RIGHT(l));
+const REST             = l=>RIGHT(RIGHT(l));
+const PUSH             = l=>x=>FOLD(l)(UNSHIFT(EMPTY)(x))(UNSHIFT);
+const RANGE            = Z(f=>m=>n=>IF(IS_LESS_OR_EQUAL(m)(n))(x=>UNSHIFT(f(INCREMENT(m))(n))(m)(x))(EMPTY));
+const FOLD             = Z(f=>l=>x=>g=>IF(IS_EMPTY(l))(x)(y=>g(f(REST(l))(x)(g))(FIRST(l))(y)));
+const MAP              = k=>f=>FOLD(k)(EMPTY)(l=>x=>UNSHIFT(l)(f(x)));
 
 
 const ONE   = INCREMENT(ZERO);
