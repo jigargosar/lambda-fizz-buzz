d0ca8bfa4c5084832f80f7e1e56b408075b07cc5
Jigar Gosar <jigar.gosar@gmail.com>
2016-10-25 15:43:22 +0530

    r

--- src/fizz-buzz.js
+++ src/fizz-buzz.js
@@ -2,17 +2,32 @@ function fizzBuzz(print) {
     for (let i = 1; i <= 100; i++) {
         if (i % 15 === 0) {
             print("FizzBuzz");
-        }else
-        if (i % 3 === 0) {
+        } else if (i % 3 === 0) {
             print("Fizz");
-        }else
-        if (i % 5 === 0) {
+        } else if (i % 5 === 0) {
             print("Buzz");
-        }else
-        print(i);
+        } else
+            print(i);
     }
 
 }
 
-fizzBuzz(console.log.bind(console))
+
+const ZERO  = (fn, x) => x,
+      ONE   = (fn, x) => fn(x),
+      TWO   = (fn, x) => fn(fn(x)),
+      THREE = (fn, x) => fn(fn(fn(x)))
+
+
+// ======= TESTS ========
+
+const log = console.log.bind(console)
+
+// fizzBuzz(log)
+const toInteger = NUMBER => NUMBER(n => n + 1, 0)
+
+log(toInteger(ZERO))
+log(toInteger(ONE))
+log(toInteger(TWO))
+log(toInteger(THREE))
 
